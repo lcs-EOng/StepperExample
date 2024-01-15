943a16084b06eb7744cde22e32a3563d0d6a208e
Eason Ong <eong@lcs.on.ca>
2024-01-15 09:14:14 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Ong Eason on 15/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
